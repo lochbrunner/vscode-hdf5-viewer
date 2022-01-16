a9bc7f667c18f660d27ea28de492c5525dddade7
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2022-01-16 15:27:10 +0100

    generates complex dataset

--- tools/generate.py
+++ tools/generate.py
@@ -2,6 +2,7 @@
 
 
 import h5py
+import numpy as np
 from pathlib import Path
 
 
@@ -17,6 +18,21 @@ def main():
         subgrp = grp.create_group("subgroup")
         subgrp.create_dataset("dataset_2", (100,), dtype='i')
         subgrp.create_dataset("dataset_3", (100,), dtype='i')
+
+        # complex dataset
+        data1 = np.arange(50.)
+        data2 = 2.0*data1
+        data3 = 3.0*data1
+        data4 = 3.0*data1
+
+        # use namesList to define dtype for recarray
+        namesList = ['height', 'mass', 'velocity', 'gravity']
+        ds_dt = np.dtype({'names': namesList, 'formats': [(float)]*4})
+
+        rec_arr = np.rec.fromarrays([data1, data2, data3, data4], dtype=ds_dt)
+        dat3 = grp.create_dataset('physics', (50,), data=rec_arr)
+        dat3.attrs['date'] = 'yesterday'
+        dat3.attrs['purpose'] = 'complex'
 
 
 if __name__ == '__main__':
